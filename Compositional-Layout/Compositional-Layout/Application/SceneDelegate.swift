97ee9394e13a06df126675f02e51bce1c9a61d79
Mini <minnnidev@gmail.com>
2022-12-12 23:40:34 +0900

    [Design] section 여러 개 Compositional Layout 구현 #1

--- Compositional-Layout/Compositional-Layout/Application/SceneDelegate.swift
+++ Compositional-Layout/Compositional-Layout/Application/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
             
-            let rootVC = GridViewController()
+            let rootVC = DistinctSectionViewController()
             
             window.rootViewController = rootVC
             window.makeKeyAndVisible()
