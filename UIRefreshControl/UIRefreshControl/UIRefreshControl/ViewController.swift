8fd9038aa619e0b3f7055bb1f52e0b996484bf77
Mini <minnnidev@gmail.com>
2023-06-25 18:34:40 +0900

    [Setting] 프로젝트 생성 및 세팅

---
+++ UIRefreshControl/UIRefreshControl/UIRefreshControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIRefreshControl
+//
+//  Created by 김민 on 2023/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .yellow
+    }
+}
+
